7db135f8cb4012f545006ad4e9625251d38017eb
franbusleiman <72284104+franbusleiman@users.noreply.github.com>
2020-12-27 17:25:03 -0300

    Closes #1

---
+++ src/test/java/franciscobusleiman/mvcProductos/mvcProductos/MvcProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package franciscobusleiman.mvcProductos.mvcProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
