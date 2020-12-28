b5c935f2b2f5467c5847c65b01b0a484b8bda5fb
franbusleiman <72284104+franbusleiman@users.noreply.github.com>
2020-12-28 10:24:44 -0300

    .

--- src/main/java/franciscobusleiman/mvcProductos/mvcProductos/controllers/ProductController.java
+++ src/main/java/franciscobusleiman/mvcProductos/mvcProductos/controllers/ProductController.java
@@ -1,2 +1,28 @@
-package franciscobusleiman.mvcProductos.mvcProductos.controllers;public class ProductController {
+package franciscobusleiman.mvcProductos.mvcProductos.controllers;
+
+import franciscobusleiman.mvcProductos.mvcProductos.domain.Product;
+import franciscobusleiman.mvcProductos.mvcProductos.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping({"", "index", "/index"})
+    public String getProducts(Model model){
+
+        Set<Product> products = productService.listAllProducts();
+
+        model.addAttribute("products", products);
+
+        return "index";
+    }
 }
