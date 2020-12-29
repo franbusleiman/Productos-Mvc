7cda3fea9f7425db39fb7f1575ec217f047f6a8e
franbusleiman <72284104+franbusleiman@users.noreply.github.com>
2020-12-29 08:31:11 -0300

    Ademas de lo indicado, se creo un archivo data.sql en el cual cargamos objetos de tipo category. Closes #10

--- src/main/java/franciscobusleiman/mvcProductos/mvcProductos/domain/Category.java
+++ src/main/java/franciscobusleiman/mvcProductos/mvcProductos/domain/Category.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 @Entity
 public class Category {
 @Id
-@GeneratedValue(strategy = GenerationType.AUTO)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO category (description) VALUES ('cocina');
+INSERT INTO category (description) VALUES ('jardin');
+INSERT INTO category (description) VALUES ('baño');
+INSERT INTO category (description) VALUES ('juguetes');
+INSERT INTO category (description) VALUES ('belleza');
